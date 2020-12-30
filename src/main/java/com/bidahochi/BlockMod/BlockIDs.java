e7e25304e05cf28b02b02a61d25cd3083696a436
hariesh <hjega14@gmail.com>
2020-12-30 09:19:03 +0000

    Fuck you, Fuck yourself Intellij And fuck you Java, Go suck one

---
+++ src/main/java/com/bidahochi/BlockMod/BlockIDs.java
@@ -0,0 +1,31 @@
+package com.bidahochi.BlockMod;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.ItemBlock;
+
+public enum BlockIDs {
+
+    bauxiteOre(false,null, "Bauxite"),
+    rawConcrete(false,null,"Concrete"),
+    peagravel_snow(false,null, "peagravelsnow"),
+    peagravel(false,null, "peagravel"),
+    although(false,null, "alothi"),
+    metalSiding_White(false,null, "metalwhite"),
+    metalSiding_LightGrey(false,null, "metalgray"),
+    metalSiding_Grey(false,null, "metalgraygary"),
+    metalSiding_Black(false,null, "metal bloack"),
+    BlockmetalSiding(false,null,"metalsiding"),
+    ;
+
+    public Block block;
+    public boolean hasItemBlock;
+    public Class itemBlockClass;
+    public String blockName;
+
+
+    BlockIDs(boolean hasItemBlock, Class<? extends ItemBlock> itemBlockClass, String blockName) {
+        this.hasItemBlock = hasItemBlock;
+        this.itemBlockClass = itemBlockClass;
+        this.blockName = blockName;
+    }
+}
--- src/main/java/com/bidahochi/BlockMod/FoxBlocks.java
+++ src/main/java/com/bidahochi/BlockMod/FoxBlocks.java
@@ -26,6 +26,7 @@ public class FoxBlocks
     public static final String VERSION = "1.1";
     public static CreativeTabs foxBlocksCreativeTab;
     public static Logger blockLogger = LogManager.getLogger("FoxBlocks");
+    
     @EventHandler
     public void preInit()
     {
