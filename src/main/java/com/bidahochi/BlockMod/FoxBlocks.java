33138c785220a608c02d8af1d8af55143a63c412
hariesh <hjega14@gmail.com>
2020-12-30 08:18:32 +0000

    Hey I fixed it by touching it

--- src/main/java/com/bidahochi/BlockMod/FoxBlocks.java
+++ src/main/java/com/bidahochi/BlockMod/FoxBlocks.java
@@ -1,10 +1,13 @@
 package com.bidahochi.BlockMod;
 
-import com.bidahochi.BlockMod.EnumBlocks.*;
+import com.bidahochi.BlockMod.blocks.metals.*;
+import com.bidahochi.BlockMod.blocks.essentiallymemes.*;
+import com.bidahochi.BlockMod.blocks.aggregates.*;
+import com.bidahochi.BlockMod.blocks.stones.*;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.registry.GameRegistry;
-import com.bidahochi.BlockMod.blocks.*;
+import com.bidahochi.BlockMod.EnumBlocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
@@ -46,25 +49,15 @@ public class FoxBlocks
 
     public void registerBlocks() {
         EnumBlocks.bauxiteOre.leBlock = new BauxiteOre(Material.rock).setHardness(1F).setStepSound(Block.soundTypeMetal);
-        EnumBlocks.bauxiteOre.bloccName = "Bauxite Ore";
         EnumBlocks.rawConcrete.leBlock = new RawConcrete(Material.rock);
-        EnumBlocks.rawConcrete.bloccName = "Raw Concrete";
         EnumBlocks.peagravel_snow.leBlock = new SnowyPeaGravel(Material.sand);
-        EnumBlocks.peagravel_snow.bloccName = "Snowy Pea Gravel";
         EnumBlocks.peagravel.leBlock = new PeaGravel(Material.sand);
-        EnumBlocks.peagravel.bloccName = "Pea Gravel";
         EnumBlocks.although.leBlock = new Although(Material.clay).setLightLevel(3F).setStepSound(Block.soundTypeAnvil).setResistance(4F).setHardness(2F);
-        EnumBlocks.although.bloccName = "Although";
         EnumBlocks.metalSiding_White.leBlock = new MetalSiding_White(Material.iron);
-        EnumBlocks.metalSiding_White.bloccName = "White Metal Siding";
         EnumBlocks.metalSiding_LightGrey.leBlock = new MetalSiding_LightGrey(Material.iron);
-        EnumBlocks.metalSiding_LightGrey.bloccName = "Light Grey Metal Siding";
         EnumBlocks.metalSiding_Grey.leBlock = new MetalSiding_Grey(Material.iron);
-        EnumBlocks.metalSiding_Grey.bloccName = "Grey Metal Siding";
         EnumBlocks.metalSiding_Black.leBlock = new MetalSiding_Black(Material.iron);
-        EnumBlocks.metalSiding_Black.bloccName = "Black Metal Siding";
         EnumBlocks.BlockmetalSiding.leBlock = new BlockmetalSiding();
-        EnumBlocks.BlockmetalSiding.bloccName = "Metal Siding";
 
 
         for (EnumBlocks blocc : EnumBlocks.values()) {
