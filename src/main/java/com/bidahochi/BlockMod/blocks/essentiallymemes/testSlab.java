d95d59ffe7192ac19d9dac29125f16678ee4752f
hariesh <hjega14@gmail.com>
2020-12-31 02:06:04 +0000

    Documentation of SourceCode

--- src/main/java/com/bidahochi/BlockMod/blocks/essentiallymemes/testSlab.java
+++ src/main/java/com/bidahochi/BlockMod/blocks/essentiallymemes/testSlab.java
@@ -1,5 +1,22 @@
 package com.bidahochi.BlockMod.blocks.essentiallymemes;
 
+/**
+ *
+ *  This file is the sourcecode for slab code!
+ *
+ *  Inside the public slab(boolean <if it is 2 high its true, else 1 high false>, Material <material>)
+ *
+ *  Writing to blockhanders
+ *  Use Format:
+ *      BlockIDs.<slab>.block = new <slab>(false, Material.<material>);
+ *
+ *
+ *  Any questions just ping or dm me on the discord!
+ *
+ *  -hariesh
+ *  
+ */
+
 import net.minecraft.block.BlockSlab;
 import net.minecraft.block.material.Material;
 
