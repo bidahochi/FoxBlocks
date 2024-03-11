a06db6fbce92555eb186e3a9f172904c158da4fb
bidahochi <agentnolan007@gmail.com>
2024-03-11 00:41:23 -0600

    fixed roadcover render boundingbox
    they should render correctly now or something idk lmk if theres a better way

--- src/main/java/com/bidahochi/BlockMod/blocks/constructionmaterials/roadcover/TileRoadCover0.java
+++ src/main/java/com/bidahochi/BlockMod/blocks/constructionmaterials/roadcover/TileRoadCover0.java
@@ -1,10 +1,13 @@
 package com.bidahochi.BlockMod.blocks.constructionmaterials.roadcover;
 
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.play.server.S35PacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.AxisAlignedBB;
 
 public class TileRoadCover0 extends TileEntity {
     public int dir=0;
@@ -55,5 +58,56 @@ public class TileRoadCover0 extends TileEntity {
         this.markDirty();
         return this;
     }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public AxisAlignedBB getRenderBoundingBox()
+    {
+        //return AxisAlignedBB.getBoundingBox(xCoord-2, yCoord-1, zCoord-1, xCoord + 1, yCoord + 1, zCoord + 1);
+
+        //int xminus = 1;
+        //int zminus = 1;
+        //int yminus = 1;
+        //int xplus = 1;
+        //int zplus = 1;
+        //int yplus =1;
+        //int aa =1;
+        int ecks =0;
+        int zee =0;
+        int why =0;
+
+        //AxisAlignedBB base = super.getRenderBoundingBox();//i have no idea what im doing
+
+        //negative xCoord is negative Z axis in game?
+        //
+        switch(dir) {
+
+            case 0:{// north
+                zee = -1;
+                //System.out.println(dir+" north");
+                break;
+            }
+            case 1: {//east
+                ecks = 1;
+                //System.out.println(dir+" east");
+                break;
+            }
+            case 2: {//south
+                zee = 1;
+                //System.out.println(dir+" south");
+                break;
+            }
+            case 3: {//west
+                ecks = -1;
+                //System.out.println(dir+" west");
+                break;
+            }
+        }
+        //System.out.println(dir+" WHAT");
+
+        //return AxisAlignedBB.getBoundingBox(xCoord-1+ecks, yCoord-1, zCoord-1+zee, xCoord + 1+ecks, yCoord + 1, zCoord + 1+zee);
+        return AxisAlignedBB.getBoundingBox(xCoord-2+ecks, yCoord-2, zCoord-2+zee, xCoord+2+ecks, yCoord+2, zCoord+2+zee);
+    }
+
 
 }
--- src/main/java/com/bidahochi/BlockMod/blocks/constructionmaterials/roadcover/TileRoadCover3.java
+++ src/main/java/com/bidahochi/BlockMod/blocks/constructionmaterials/roadcover/TileRoadCover3.java
@@ -1,10 +1,13 @@
 package com.bidahochi.BlockMod.blocks.constructionmaterials.roadcover;
 
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.play.server.S35PacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.AxisAlignedBB;
 
 public class TileRoadCover3 extends TileEntity {
     public int dir=0;
@@ -54,6 +57,36 @@ public class TileRoadCover3 extends TileEntity {
         dir=(byte) direction;
         this.markDirty();
         return this;
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public AxisAlignedBB getRenderBoundingBox()
+    {
+        int ecks =0;
+        int zee =0;
+        int why =0;
+
+        switch(dir) {
+
+            case 0:{// north
+                zee = -1;
+                break;
+            }
+            case 1: {//east
+                ecks = 1;
+                break;
+            }
+            case 2: {//south
+                zee = 1;
+                break;
+            }
+            case 3: {//west
+                ecks = -1;
+                break;
+            }
+        }
+        return AxisAlignedBB.getBoundingBox(xCoord-2+ecks, yCoord-2, zCoord-2+zee, xCoord+2+ecks, yCoord+2, zCoord+2+zee);
     }
 
 }
