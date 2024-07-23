647e9e8d2245af437880fef13e0da8a607ea0260
TBEA <74006945+RealTBEA@users.noreply.github.com>
2024-07-23 19:47:22 -0400

    fixing the damn roads
    fixing the damn roads

---
+++ src/main/java/com/bidahochi/BlockMod/blocks/roadpaints/TileEntitySpecialRenderer/BaseRoadPaintBlock.java
@@ -0,0 +1,11 @@
+package com.bidahochi.BlockMod.blocks.roadpaints.TileEntitySpecialRenderer;
+
+import com.bidahochi.BlockMod.render.models.Modelrpb_full;
+
+public class BaseRoadPaintBlock extends BaseRoadPaintTileEntitySpecialRenderer
+{
+    public BaseRoadPaintBlock()
+    {
+        setModel(new Modelrpb_full());
+    }
+}
---
+++ src/main/java/com/bidahochi/BlockMod/blocks/roadpaints/TileEntitySpecialRenderer/BaseRoadPaintSlab.java
@@ -0,0 +1,11 @@
+package com.bidahochi.BlockMod.blocks.roadpaints.TileEntitySpecialRenderer;
+
+import com.bidahochi.BlockMod.render.models.Modelrpb_half;
+
+public class BaseRoadPaintSlab extends BaseRoadPaintTileEntitySpecialRenderer
+{
+    public BaseRoadPaintSlab()
+    {
+        setModel(new Modelrpb_half());
+    }
+}
