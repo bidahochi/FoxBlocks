e4a335f457f9d17d2c1ea117a263a585a1ccae47
TBEA <74006945+RealTBEA@users.noreply.github.com>
2024-07-22 22:35:17 -0400

    TileWrappedWood Conversion

--- src/main/java/com/bidahochi/BlockMod/blocks/lumber/TileWrappedWood1.java
+++ src/main/java/com/bidahochi/BlockMod/blocks/lumber/TileWrappedWood1.java
@@ -1,58 +1,17 @@
 package com.bidahochi.BlockMod.blocks.lumber;
 
 
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.network.NetworkManager;
-import net.minecraft.network.play.server.S35PacketUpdateTileEntity;
-import net.minecraft.tileentity.TileEntity;
+import com.bidahochi.BlockMod.blocks.BaseClassFolder.BaseTileEntity;
 
-public class TileWrappedWood1 extends TileEntity {
-    public int dir=0;
-
-    public TileWrappedWood1(int dir) {
-        super();
-        this.dir = dir;
+public class TileWrappedWood1 extends BaseTileEntity
+{
+    public TileWrappedWood1(int dir)
+    {
+        super(dir);
     }
 
-    public TileWrappedWood1() {}
+    public TileWrappedWood1()
+    {
 
-    @Override
-    public boolean shouldRenderInPass(int pass){ return pass==0; }
-
-
-    @Override
-    public S35PacketUpdateTileEntity getDescriptionPacket() {
-        NBTTagCompound nbttagcompound = new NBTTagCompound();
-        writeToNBT(nbttagcompound);
-        return new S35PacketUpdateTileEntity(this.xCoord, this.yCoord, this.zCoord, 0, nbttagcompound);
     }
-
-    @Override
-    public void onDataPacket(NetworkManager net, S35PacketUpdateTileEntity pkt) {
-        if(pkt ==null){return;}
-        readFromNBT(pkt.func_148857_g());
-        markDirty();
-    }
-
-    @Override
-    public void writeToNBT(NBTTagCompound tag){
-        super.writeToNBT(tag);
-        tag.setInteger("direction", dir);
-    }
-
-    @Override
-    public void readFromNBT(NBTTagCompound tag){
-        super.readFromNBT(tag);
-        dir = tag.getInteger("direction");
-        if(worldObj!=null && worldObj.isRemote) {
-            markDirty();
-        }
-    }
-
-    public TileWrappedWood1 setFacing(int direction){
-        dir=(byte) direction;
-        this.markDirty();
-        return this;
-    }
-
 }
--- src/main/java/com/bidahochi/BlockMod/blocks/lumber/TileWrappedWood3.java
+++ src/main/java/com/bidahochi/BlockMod/blocks/lumber/TileWrappedWood3.java
@@ -1,58 +1,17 @@
 package com.bidahochi.BlockMod.blocks.lumber;
 
 
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.network.NetworkManager;
-import net.minecraft.network.play.server.S35PacketUpdateTileEntity;
-import net.minecraft.tileentity.TileEntity;
+import com.bidahochi.BlockMod.blocks.BaseClassFolder.BaseTileEntity;
 
-public class TileWrappedWood3 extends TileEntity {
-    public int dir=0;
-
-    public TileWrappedWood3(int dir) {
-        super();
-        this.dir = dir;
+public class TileWrappedWood3 extends BaseTileEntity
+{
+    public TileWrappedWood3(int dir)
+    {
+        super(dir);
     }
 
-    public TileWrappedWood3() {}
+    public TileWrappedWood3()
+    {
 
-    @Override
-    public boolean shouldRenderInPass(int pass){ return pass==0; }
-
-
-    @Override
-    public S35PacketUpdateTileEntity getDescriptionPacket() {
-        NBTTagCompound nbttagcompound = new NBTTagCompound();
-        writeToNBT(nbttagcompound);
-        return new S35PacketUpdateTileEntity(this.xCoord, this.yCoord, this.zCoord, 0, nbttagcompound);
     }
-
-    @Override
-    public void onDataPacket(NetworkManager net, S35PacketUpdateTileEntity pkt) {
-        if(pkt ==null){return;}
-        readFromNBT(pkt.func_148857_g());
-        markDirty();
-    }
-
-    @Override
-    public void writeToNBT(NBTTagCompound tag){
-        super.writeToNBT(tag);
-        tag.setInteger("direction", dir);
-    }
-
-    @Override
-    public void readFromNBT(NBTTagCompound tag){
-        super.readFromNBT(tag);
-        dir = tag.getInteger("direction");
-        if(worldObj!=null && worldObj.isRemote) {
-            markDirty();
-        }
-    }
-
-    public TileWrappedWood3 setFacing(int direction){
-        dir=(byte) direction;
-        this.markDirty();
-        return this;
-    }
-
 }
