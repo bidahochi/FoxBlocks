a8a37a0c24881638e3f3aeaea3a8fb0b9e573248
TBEA <74006945+RealTBEA@users.noreply.github.com>
2024-07-22 22:23:43 -0400

    Part 2
    MORE CONVERSIONS

--- src/main/java/com/bidahochi/BlockMod/blocks/overhead/TileOverhead_pole_slope_6.java
+++ src/main/java/com/bidahochi/BlockMod/blocks/overhead/TileOverhead_pole_slope_6.java
@@ -1,6 +1,7 @@
 package com.bidahochi.BlockMod.blocks.overhead;
 
 
+import com.bidahochi.BlockMod.blocks.BaseClassFolder.BaseTileEntity;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.nbt.NBTTagCompound;
@@ -9,53 +10,16 @@ import net.minecraft.network.play.server.S35PacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.AxisAlignedBB;
 
-public class TileOverhead_pole_slope_6 extends TileEntity {
-    public int dir=0;
-
-    public TileOverhead_pole_slope_6(int dir) {
-        super();
-        this.dir = dir;
+public class TileOverhead_pole_slope_6 extends BaseTileEntity
+{
+    public TileOverhead_pole_slope_6(int dir)
+    {
+        super(dir);
     }
 
-    public TileOverhead_pole_slope_6() {}
+    public TileOverhead_pole_slope_6()
+    {
 
-    @Override
-    public boolean shouldRenderInPass(int pass){ return pass==0; }
-
-
-    @Override
-    public S35PacketUpdateTileEntity getDescriptionPacket() {
-        NBTTagCompound nbttagcompound = new NBTTagCompound();
-        writeToNBT(nbttagcompound);
-        return new S35PacketUpdateTileEntity(this.xCoord, this.yCoord, this.zCoord, 0, nbttagcompound);
-    }
-
-    @Override
-    public void onDataPacket(NetworkManager net, S35PacketUpdateTileEntity pkt) {
-        if(pkt ==null){return;}
-        readFromNBT(pkt.func_148857_g());
-        markDirty();
-    }
-
-    @Override
-    public void writeToNBT(NBTTagCompound tag){
-        super.writeToNBT(tag);
-        tag.setInteger("direction", dir);
-    }
-
-    @Override
-    public void readFromNBT(NBTTagCompound tag){
-        super.readFromNBT(tag);
-        dir = tag.getInteger("direction");
-        if(worldObj!=null && worldObj.isRemote) {
-            markDirty();
-        }
-    }
-
-    public TileOverhead_pole_slope_6 setFacing(int direction){
-        dir=(byte) direction;
-        this.markDirty();
-        return this;
     }
 
     @SideOnly(Side.CLIENT)
