e8c733a1847a47658e88f1fcf0776b393d620a51
TBEA <74006945+RealTBEA@users.noreply.github.com>
2025-07-17 18:38:28 -0400

    Update BaseItemWallBlock.java
    * Added Flavor Text

--- src/main/java/com/bidahochi/BlockMod/items/BaseItems/BaseItemWallBlock.java
+++ src/main/java/com/bidahochi/BlockMod/items/BaseItems/BaseItemWallBlock.java
@@ -1,8 +1,14 @@
 package com.bidahochi.BlockMod.items.BaseItems;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemBlockWithMetadata;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumChatFormatting;
+
+import java.util.List;
 
 public class BaseItemWallBlock extends ItemBlockWithMetadata
 {
@@ -14,5 +20,12 @@ public class BaseItemWallBlock extends ItemBlockWithMetadata
     public String getUnlocalizedName(ItemStack item)
     {
         return this.getUnlocalizedName() + "_" + item.getItemDamage();
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void addInformation(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, List list, boolean par4)
+    {
+        list.add("\u00a77" + EnumChatFormatting.GREEN + "Modern Minecraft Styled Wall");
     }
 }
